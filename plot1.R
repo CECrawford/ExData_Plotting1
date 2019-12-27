4dc8cac4bf354986caf43beaf4137129f3688143
CE Crawford <charlotte_developer@hotmail.com>
2019-12-27 14:48:44 +1300

    minor edit to comments

--- plot1.R
+++ plot1.R
@@ -1,4 +1,7 @@
 #load the raw data
+#script assumes that you have downloaded and unpacked the "household_power_consumption.txt" 
+#into your working directory
+
 rawData <- read.csv("household_power_consumption.txt", header = TRUE, stringsAsFactors = FALSE, sep = ";")
 #drop the time column as it is not needed
 pcData <- select(rawData, -(Time))
